a96be1ebac6ae8e65c2280d0c1e5825cc6ff8f92
AaronKWalker <aaron.k.walker1@gmail.com>
2017-10-08 16:03:37 -0500

    switched to new cli table and completed customer section

--- bamazon.js
+++ bamazon.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const Table = require('cli-table');
+//const Table = require('cli-table');
 const chalk = require('chalk');
+const Table = require('cli-table-redemption');
+
+let userStatus, itemIndex;
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -11,6 +14,8 @@ const connection = mysql.createConnection({
   database: 'bamazonMkII'
 });
 
+
+//======[UTILITY FUNCTIONS]======
 function connectToDB(){
   connection.connect(function(err){
     if (err) throw err;
@@ -18,6 +23,158 @@ function connectToDB(){
     displayWelcome();
   });
 }
+
+
+function updateInventory(param1, param2){
+  connection.query('UPDATE inventory SET ? WHERE ?', [
+    {
+      stock_quantity: param1
+    }, {
+      item_id: param2
+    }
+  ], function(err){
+    if (err) throw err;
+    console.log('db updated');
+  });
+}
+
+
+function stayOrGo(answer){
+  switch (userStatus) {
+    case 'customer':
+      inquirer.prompt([
+        {
+          name: 'userChoice',
+          type: 'confirm',
+          message: `You bought ${answer.amount} ${answer.item}.  Your total is $${answer.cost}.00. Would you like to buy anything else?`
+        }
+      ]).then(function(answer){
+        answer.userChoice ? customer() : displayWelcome();
+      });
+      break;
+  }
+}
+
+
+function showInv(input){
+  if (input === 'all') {
+    connection.query('SELECT * FROM inventory', function(error, results){
+      if (error) throw error;
+
+      let table = new Table({
+        head: ['No.', 'Product', 'Department', 'Price', 'Stock Quanity'],
+        colWidths: [5, 15, 15, 10, 15]
+      });
+      for (let i = 0; i < results.length; i++) {
+        table.push(
+          [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity, results[i].product_sales]
+        );
+      }
+      console.log(table.toString());
+      manager();
+    });//--connection.query--
+  }
+
+  if (input === 'low') {
+    connection.query('SELECT * FROM inventory WHERE stock_quantity<=5', function(error, results){
+      if (error) throw error;
+
+      let table = new Table({
+        head: ['No.', 'Product', 'Department', 'Price', 'Stock Quanity'],
+        colWidths: [5, 15, 15, 10, 15]
+      });
+      for (let i = 0; i < results.length; i++) {
+        table.push(
+          [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity, results[i].product_sales]
+        );
+      }
+      console.log(table.toString());
+      manager();
+    });//--connection.query--
+  }
+}
+
+
+function manageInv(input){
+  if (input === 'increase') {
+    let choiceArr = [];
+    connection.query('SELECT * FROM  inventory', function(error, results){
+      if (error) throw error;
+      for (var i = 0; i < results.length; i++) {
+        choiceArr.push(results[i].product_name);
+      }
+
+      let table = new Table({
+        head: ['No.', 'Product', 'Department', 'Price', 'Stock Quanity'],
+        colWidths: [5, 15, 15, 10, 15]
+      });
+      for (let i = 0; i < results.length; i++) {
+        table.push(
+          [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity, results[i].product_sales]
+        );
+      }
+      console.log(table.toString());
+
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'userChoice',
+          message: 'Which item do you want to increase? Please input item No.'
+        }, {
+          type: 'input',
+          name: 'amount',
+          message: 'How much do you want to increase by?'
+        }, {
+          type: 'confirm',
+          name: 'validation',
+          message: 'Are you sure?'
+        }
+      ]).then(function(answer){
+        if (answer.validation) {
+          let index = parseInt(answer.userChoice) - 1;
+          let newAmount = parseInt(results[index].stock_quantity + answer.amount);
+          connection.query('UPDATE inventory SET ? WHERE ?'[
+            {
+              stock_quantity: newAmount
+            }, {
+              item_id: answer.userChoice
+            }
+          ],function(error,){
+            if (error) throw error;
+            manager();
+          })
+        } else {
+          manageInv('increase');
+        }
+      });
+    });
+
+  }
+
+  if (input === 'add') {
+    inquirer.prompt([
+      {
+        type: 'rawlist',
+        name: 'userChoice',
+        message: 'Which item do you want to increase?',
+        choices: function(){
+          let choiceArr = [];
+          connection.query('SELECT * FROM  inventory', function(error, results){
+            if (error) throw error;
+            for (var i = 0; i < results.length; i++) {
+              choiceArr.push(results[i].product_name);
+              return choiceArr;
+            }
+          });
+          return choiceArr;
+        }
+      }
+    ]).then(function(answer){
+
+    });
+  }
+}
+
 
 //display the welcome/type of user screen
 function displayWelcome(){
@@ -43,7 +200,7 @@ function displayWelcome(){
     "||    ||  ||    ||  ||      ||  ||    ||  ||        ||    ||  ||      ||"
   ));
   console.log(chalk.blue.bold(
-    "||||||    ||    ||  ||      ||  ||    ||  |||||||   ||||||||  ||      ||"
+    "||||||    ||    ||  ||      ||  ||    ||  ||||||||  ||||||||  ||      ||"
   ));
   console.log(chalk.yellow.bold(
     "////////////////////////////////////////////////////////////////////////"
@@ -58,41 +215,98 @@ function displayWelcome(){
   ]).then(function(answer){
     switch (answer.userChoice) {
       case 'Customer':
+        userStatus = 'customer';
         customer();
         break;
       case 'Manager':
+        userStatus = 'manager';
         manager();
         break;
       case 'Supervisor':
+        userStatus = 'supervisor';
         supervisor();
         break;
     }
   });
 }
 
+
 //IF costumer//////////////////
 function customer(){
-  console.log('customer');
-}
-  //diplay items available
+  connection.query('SELECT * FROM inventory', function(error, results){
+    if (error) throw error;
+    let table = new Table({
+      head: ['No.', 'Product', 'Department', 'Price', 'Stock Quanity'],
+      colWidths: [5, 15, 15, 10, 15]
+    });
 
-  //take user input
+    for (let i = 0; i < results.length; i++) {
+      table.push(
+        [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity, results[i].product_sales]
+      );
+    }
+    console.log(table.toString());
 
-  //check if quanity requested is available
 
-  //change amount of item in DB
+    inquirer.prompt([
+      {
+        name: 'userChoice',
+        type:'input',
+        message: 'Please select an item to buy via its No.'
+      }, {
+        name: 'amount',
+        type: 'input',
+        message: 'How many would you like?'
+      }
+    ]).then(function(answer){
+      itemIndex = parseInt(answer.userChoice) - 1;
+      if (parseInt(results[itemIndex].stock_quantity - answer.amount) <= 0) {
+        console.log(chalk.white.bgRed.bold('Not enough inventory available! Please make another selection'));
+        customer();
+      } else {
+        var newAmount = parseInt(results[itemIndex].stock_quantity - answer.amount);
+        updateInventory(newAmount, answer.userChoice);
+        answer.item = results[itemIndex].product_name;
+        answer.cost = parseInt(results[itemIndex].price * answer.amount);
+      }
+      return answer;
+    }).then(function(answer){
+      stayOrGo(answer);
+    });//--.then--
+  });//--connection.query--
+}//--customer--
 
-  //diplay items again
 
 //IF manager//////////////////
 function manager(){
-  console.log('manager');
+  inquirer.prompt([
+    {
+      name: 'userChoice',
+      type:'rawlist',
+      message: 'Please make a selection',
+      choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Go Back']
+    },
+  ]).then(function(answer){
+    switch (answer.userChoice) {
+      case 'View Products for Sale':
+        showInv('all');
+        break;
+      case 'View Low Inventory':
+        showInv('low');
+        break;
+      case 'Add to Inventory':
+        manageInv('increase');
+        break;
+      case 'Add New Product':
+        manageInv('add');
+        break;
+      case 'Go Back':
+        displayWelcome();
+        break;
+    }
+  });
 }
-  //display options
 
-  //take user input
-
-  //do whatever the option selected will do
 
 
 //IF supervisor//////////////////
